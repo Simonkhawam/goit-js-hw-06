d9a5c0bd2393bafc373e2d99595cfca1abd2ae14
Simon Khawam <khawam.simon@me.com>
2023-11-02 15:29:59 +0200

    commit change for now

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+const decrementButton = document.querySelector(
+    'button[data-action="decrement"]'
+);
+const incrementButton = document.querySelector(
+    'button[data-action="increment"]'
+);
+const valueSpan = document.getElementById("value");
+
+let counterValue = 0;
+
+decrementButton.addEventListener("click", () => {
+    counterValue--;
+    valueSpan.textContent = counterValue;
+});
+
+incrementButton.addEventListener("click", () => {
+    counterValue++;
+    valueSpan.textContent = counterValue;
+});
